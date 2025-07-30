8b53b52a7605f86b990a08e1e9d426229aa97562
Bhenny Hayman <bengero686@gmail.com>
2025-07-30 13:38:03 +0000

    new changes

--- components/SearchedBlog.jsx
+++ components/SearchedBlog.jsx
@@ -20,7 +20,7 @@ function SearchedBlog({blogs, setBlogs}) {
       {filteredBlogs.map((blog, index) => (
         <div key={index} className='p-4 m-2 mb-15 shadow-grey-100 shadow-lg rounded-lg'>
           <h2 className='text-xl font-bold text-blue-600'>Title: {blog.title}</h2>
-          <p className='m-2'>{blog.content}</p>
+          <p className='m-2'>{blog.content}</p> 
           <p className='text-sm text-gray-500'>Created at: {new Date(blog.createdAt).toLocaleDateString()}</p>
         </div>
       ))}
